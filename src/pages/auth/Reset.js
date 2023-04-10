0efd6c0c90cad897f14d34d7258160a0cdb3c050
Kaela Nguyen <kaedtn96@gmail.com>
2023-04-09 22:00:50 -0700

    Add functionality to reset password page

--- src/pages/auth/Reset.js
+++ src/pages/auth/Reset.js
@@ -2,33 +2,64 @@ import { Link } from "react-router-dom";
 import resetImg from "../../assets/forgot.png";
 import Card from "../../components/card/Card";
 import "./auth.css";
+import { useState } from "react";
+import { sendPasswordResetEmail } from "firebase/auth";
+import { auth } from "../../firebase/config";
+import { toast } from "react-toastify";
+import Loader from "../../components/loader/Loader";
 
 const Reset = () => {
+  const [email, setEmail] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const resetPassword = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        toast.success("Check your email for a reset link!");
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        toast.error(error.message);
+      });
+  };
   return (
-    <section className="login-container">
-      <div className="login-img-container">
-        <img src={resetImg} alt="Reset Password" width="500" />
-      </div>
-      <Card className="login-card">
-        <div className="login-form">
-          <h2>Reset Password</h2>
-          <form action="">
-            <input type="email" placeholder="Email" required />
-            <button className="btn btn-block btn-primary">
-              Reset Password
-            </button>
-            <div className="reset-links">
-              <p>
-                <Link to="/login">Login</Link>
-              </p>
-              <p>
-                <Link to="/register">Register</Link>
-              </p>
-            </div>
-          </form>
+    <>
+      {isLoading && <Loader />}
+      <section className="login-container">
+        <div className="login-img-container">
+          <img src={resetImg} alt="Reset Password" width="500" />
         </div>
-      </Card>
-    </section>
+        <Card className="login-card">
+          <div className="login-form">
+            <h2>Reset Password</h2>
+            <form onSubmit={resetPassword}>
+              <input
+                type="email"
+                placeholder="Email"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <button type="submit" className="btn btn-block btn-primary">
+                Reset Password
+              </button>
+              <div className="reset-links">
+                <p>
+                  <Link to="/login">Login</Link>
+                </p>
+                <p>
+                  <Link to="/register">Register</Link>
+                </p>
+              </div>
+            </form>
+          </div>
+        </Card>
+      </section>
+    </>
   );
 };
 
