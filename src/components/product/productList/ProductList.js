4b9445ad3bd7c1da2b9121f0baa87a7e0cc94129
Kaela Nguyen <kaedtn96@gmail.com>
2023-04-19 15:49:55 -0700

    Create top part of product list component

--- src/components/product/productList/ProductList.js
+++ src/components/product/productList/ProductList.js
@@ -1,7 +1,38 @@
-import React from "react";
+import { useState } from "react";
+import { BsFillGridFill } from "react-icons/bs";
+import { FaListAlt } from "react-icons/fa";
 
 const ProductList = () => {
-  return <div>ProductList</div>;
+  const [grid, setGrid] = useState(true);
+
+  return (
+    <div className="product-list">
+      <div className="top">
+        <div className="icons">
+          <BsFillGridFill size={22} onClick={() => setGrid(true)} />
+          <FaListAlt size={24} onClick={() => setGrid(false)} />
+          <p>
+            <strong>10</strong> Products Found
+          </p>
+        </div>
+        {/* Search Icon */}
+        <div>
+          <p>Search</p>
+        </div>
+        {/* Sort Products */}
+        <div className="sort">
+          <label>Sort by:</label>
+          <select>
+            <option value="latest">Latest</option>
+            <option value="lowest-price">Lowest Price</option>
+            <option value="highest-price">Highest Price</option>
+            <option value="a-z">A - Z</option>
+            <option value="z-a">Z - A</option>
+          </select>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductList;
