19c4b86b70d587016bfc42676ea2612d2915e74d
Kaela Nguyen <kaedtn96@gmail.com>
2023-04-19 15:49:30 -0700

    Set state for product page

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,8 +1,26 @@
 import "./Product.css";
 import ProductFilter from "../../components/product/productFilter/ProductFilter";
 import ProductList from "../../components/product/productList/ProductList";
+import useFetchCollection from "../../customHooks/useFetchCollection";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import {
+  STORE_PRODUCTS,
+  selectProducts,
+} from "../../redux/features/productSlice";
 
 const Product = () => {
+  const { data, isLoading } = useFetchCollection("products");
+  const products = useSelector(selectProducts);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(
+      STORE_PRODUCTS({
+        products: data,
+      })
+    );
+  }, [dispatch, data]);
   return (
     <section>
       <div className="container">
@@ -10,7 +28,7 @@ const Product = () => {
           <ProductFilter />
         </aside>
         <div className="content">
-          <ProductList />
+          <ProductList products={products} />
         </div>
       </div>
     </section>
