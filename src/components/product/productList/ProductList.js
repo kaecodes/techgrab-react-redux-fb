fb3de95d5013a9475cb915626409a8765facce6b
Kaela Nguyen <kaedtn96@gmail.com>
2023-04-20 15:13:31 -0700

    Add sort functionality of product page

--- src/components/product/productList/ProductList.js
+++ src/components/product/productList/ProductList.js
@@ -1,13 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { BsFillGridFill } from "react-icons/bs";
 import { FaListAlt } from "react-icons/fa";
 import Search from "../../search/Search";
 import "../Product.css";
 import ProductItem from "../productItem/ProductItem";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  FILTER_BY_SEARCH,
+  SORT_PRODUCTS,
+} from "../../../redux/features/filterSlice";
+import { selectFilteredProducts } from "../../../redux/features/filterSlice";
 
 const ProductList = ({ products }) => {
   const [grid, setGrid] = useState(true);
   const [search, setSearch] = useState("");
+  const [sort, setSort] = useState("latest");
+
+  const filteredProducts = useSelector(selectFilteredProducts);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(SORT_PRODUCTS({ products, sort }));
+  }, [dispatch, products, sort]);
+
+  useEffect(() => {
+    dispatch(FILTER_BY_SEARCH({ search, products }));
+  }, [dispatch, search, products]);
 
   return (
     <div className="product-list">
@@ -16,7 +35,7 @@ const ProductList = ({ products }) => {
           <BsFillGridFill size={22} onClick={() => setGrid(true)} />
           <FaListAlt size={24} onClick={() => setGrid(false)} />
           <p>
-            <strong>10</strong> Products Found
+            <strong>{filteredProducts.length}</strong> Products Found
           </p>
         </div>
         {/* Search Icon */}
@@ -26,7 +45,7 @@ const ProductList = ({ products }) => {
         {/* Sort Products */}
         <div className="sort">
           <label>Sort by:</label>
-          <select>
+          <select value={sort} onChange={(e) => setSort(e.target.value)}>
             <option value="latest">Latest</option>
             <option value="lowest-price">Lowest Price</option>
             <option value="highest-price">Highest Price</option>
@@ -40,7 +59,7 @@ const ProductList = ({ products }) => {
           <p>No Product Found</p>
         ) : (
           <>
-            {products.map((product) => {
+            {filteredProducts.map((product) => {
               return (
                 <div key={product.id} className="single-product">
                   <ProductItem {...product} grid={grid} product={product} />
