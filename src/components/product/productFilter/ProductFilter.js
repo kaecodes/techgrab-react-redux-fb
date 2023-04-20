e1d81825d0fcc2496e01a56b2e9f6e524b0583ac
Kaela Nguyen <kaedtn96@gmail.com>
2023-04-20 14:27:51 -0700

    Update styles for product filter

--- src/components/product/productFilter/ProductFilter.js
+++ src/components/product/productFilter/ProductFilter.js
@@ -3,22 +3,22 @@ import React from "react";
 const productFilter = () => {
   return (
     <div className="filter">
-      <h4>Categories</h4>
+      <h3>Categories</h3>
       <div className="choose-categories">
         <button>All Products</button>
       </div>
-      <h4>Brand</h4>
+      <h3>Brand</h3>
       <div className="brands">
         <select name="brands">
           <option value="all">All Products</option>
         </select>
       </div>
-      <h4>Price</h4>
+      <h3>Price</h3>
       <p>1500</p>
       <div className="price-slider">
         <input type="range" name="price" min="100" max="1000" />
       </div>
-      <button className="btn btn-primary">Clear Filters</button>
+      <button className="btn btn-primary clear">Clear Filters</button>
     </div>
   );
 };
