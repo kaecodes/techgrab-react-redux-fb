801b1638dbb396bfd63fca1d8e06afbe7c9e2c65
Kaela Nguyen <kaedtn96@gmail.com>
2023-04-19 21:51:36 -0700

    Add content and product details to each product card

--- src/components/product/productItem/ProductItem.js
+++ src/components/product/productItem/ProductItem.js
@@ -2,19 +2,28 @@ import { Link } from "react-router-dom";
 import Card from "../../card/Card";
 
 const ProductItem = ({ product, grid, id, name, price, desc, imageURL }) => {
+  const shortenText = (text, n) => {
+    if (text.length > n) {
+      const shortenedText = text.substring(0, n).concat("...");
+      return shortenedText;
+    }
+    return text;
+  };
+
   return (
     <Card className={grid ? "grid-view" : "list-view"}>
       <Link to={`/product-details`}>
         <div className="img">
           <img src={imageURL} alt={name} />
-          {console.log(imageURL)}
         </div>
       </Link>
       <div className="content">
         <div className="details">
           <p>{`$${price}`}</p>
-          <h4>{name}</h4>
+          <h4>{shortenText(name, 18)}</h4>
         </div>
+        {!grid && <p className="desc">{shortenText(desc, 200)}</p>}
+        <button className="btn btn-primary">Add to Cart</button>
       </div>
     </Card>
   );
